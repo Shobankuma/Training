46b91bbf61c38a3252517b53d9fa5ebd5ce43da2
Shobankuma <116084057+Shobankuma@users.noreply.github.com>
2024-11-11 18:40:31 +0530

    Create DoWhileLoop.cs

---
+++ TrainingExcercise/DoWhileLoop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingExcercise
+{
+    internal class DoWhileLoop
+    {
+        public void Run() { 
+        int i = 0;
+        // Print numbers 0 to 4 using do-while loop
+        do
+        {
+            Console.WriteLine(i);  // Prints 0, 1, 2, 3, 4
+            i++;  // Increment i after each iteration
+        } while (i< 5);
+    }
+
+    }
+}
