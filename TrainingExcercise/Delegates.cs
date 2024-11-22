0f185c05b6baa41cf2e563253abe141a700179af
Shobankuma <116084057+Shobankuma@users.noreply.github.com>
2024-11-22 14:44:43 +0530

    Delegates

---
+++ TrainingExcercise/Delegates.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingExcercise
+{
+    internal class Delegates
+    { 
+        // Define the delegate type
+        delegate void GreetDelegate(string name);
+
+        public void Run()
+        {
+            // Instantiate a delegate and associate it with a method
+            GreetDelegate greet = new GreetDelegate(SayHello);
+
+            // Invoke the delegate
+            greet("Alice");
+
+            // Delegate can also reference another method
+            GreetDelegate greet2 = new GreetDelegate(SayGoodbye);
+            greet2("Bob");
+        }
+
+        static void SayHello(string name)
+        {
+            Console.WriteLine("Hello, " + name);
+        }
+
+        static void SayGoodbye(string name)
+        {
+            Console.WriteLine("Goodbye, " + name);
+        }
+    }
+
+}
---
+++ TrainingExcercise/MulticastDelegates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingExcercise
+{
+    public delegate void RectDelegate(double width, double hight);
+    internal class MulticastDelegates
+    {
+        public void Run()
+        {
+            Rectanglee rectanglee = new Rectanglee();
+            RectDelegate obj = rectanglee.GetArea;  //binding method
+            obj += rectanglee.GetPerimeter;
+            obj(10, 20);  // or obj.Invoke(10,20) we can use invoke method
+            
+            rectanglee.GetPerimeter(10, 20);
+        }
+    }
+    class Rectanglee
+    {
+        public void GetArea(double width, double hight)
+        {
+            Console.WriteLine(width * hight);
+        }
+        public void GetPerimeter(double width, double hight)
+        {
+            Console.WriteLine(width + hight);
+        }
+    }
+}
