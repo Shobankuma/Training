1a8ddf71e6e4505bbc798620f9adf6662e047efd
Shobankuma <116084057+Shobankuma@users.noreply.github.com>
2025-01-06 11:47:33 +0530

    File I/O in C#
    File I/O examples

---
+++ TrainingExcercise/FileReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace TrainingExcercise
+{
+    internal class FileReader
+    {
+        public void Run()
+        {
+            string filePath = @"D:\Training\TrainingExcercise\example.txt";
+
+            // Create a StreamReader to read the file
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
---
+++ TrainingExcercise/FileWriter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingExcercise
+{
+    internal class FileWriter
+    {
+        public void Run()
+        {
+            string filePath = @"D:\Training\TrainingExcercise\example.txt";
+            // Create a StreamWriter to write to the file
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine("Hello, World!");
+                writer.WriteLine("This is a sample text written to the file.");
+            } 
+        }
+    }
+}
